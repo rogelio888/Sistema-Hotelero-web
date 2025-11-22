fa338c8d55956c115fdb3c152a34e478d975c7fa
Rogelio Hinojosa <rogeliovladimir2016@gmail.com>
2025-11-22 14:13:01 -0400

    Corregir URL de API hardcodeada

--- resources/js/axios.js
+++ resources/js/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crear instancia de axios
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: import.meta.env.VITE_API_URL || '/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
